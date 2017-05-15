39026b3951e48f419be4243685355ae5977d972f
Oleg Nikitashin <olegnikitashin@gmail.com>
2017-05-15 19:00:55 +0300

    Add Pages Controller
    - Add Pages Controller
    - Add Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :blogs
-
   root to: 'blogs#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'pages/home'
+  get 'pages/about'
+  get 'pages/contact'
+  resources :blogs
 end
