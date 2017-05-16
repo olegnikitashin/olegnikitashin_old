8de65112bb2a03bb6d677f97fd669689363bdd58
Oleg Nikitashin <olegnikitashin@gmail.com>
2017-05-16 18:59:40 +0300

    Portfolio feature
    - add views
    - add PortfoliosController Actions
    - add FriendlyID
    - add custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'blogs#index'
+  root to: 'pages#home'
 
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
   resources :blogs
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 end
