2614bdec7a19ec1ac79caa88728a8895fcc12765
Oleg Nikitashin <olegnikitashin@gmail.com>
2017-05-18 13:44:36 +0300

    Add CurrentUser Concern
    - implement Null Object Pattern to current_user
    - add session[:source] to store the information about user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login',
-                                            sign_out: 'logout',
-                                            sign_up: 'register' }
+                                             sign_out: 'logout',
+                                             sign_up: 'register' }
   root to: 'pages#home'
 
   get 'about', to: 'pages#about'
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
+  
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 end
