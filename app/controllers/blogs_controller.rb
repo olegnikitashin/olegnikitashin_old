ffdba09c968d08f19e588afc936d40df25945ed9
Oleg Nikitashin <olegnikitashin@gmail.com>
2017-05-19 13:38:54 +0300

    Refactor partials

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,58 +2,44 @@ class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
   layout 'blog'
 
-  # GET /blogs
-  # GET /blogs.json
   def index
     @blogs = Blog.all
   end
 
-  # GET /blogs/1
-  # GET /blogs/1.json
-  def show
-  end
+  def show; end
 
-  # GET /blogs/new
   def new
     @blog = Blog.new
   end
 
-  # GET /blogs/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /blogs
-  # POST /blogs.json
   def create
     @blog = Blog.new(blog_params)
 
     respond_to do |format|
       if @blog.save
-        format.html {redirect_to @blog, notice: 'Blog was successfully created.'}
-        format.json {render :show, status: :created, location: @blog}
+        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
+        format.json { render :show, status: :created, location: @blog }
       else
-        format.html {render :new}
-        format.json {render json: @blog.errors, status: :unprocessable_entity}
+        format.html { render :new }
+        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /blogs/1
-  # PATCH/PUT /blogs/1.json
   def update
     respond_to do |format|
       if @blog.update(blog_params)
-        format.html {redirect_to @blog, notice: 'Blog was successfully updated.'}
-        format.json {render :show, status: :ok, location: @blog}
+        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
+        format.json { render :show, status: :ok, location: @blog }
       else
-        format.html {render :edit}
-        format.json {render json: @blog.errors, status: :unprocessable_entity}
+        format.html { render :edit }
+        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /blogs/1
-  # DELETE /blogs/1.json
   def destroy
     @blog.destroy
     respond_to do |format|
