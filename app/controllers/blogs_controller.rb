cff4f6dc634b0453352f18beeeecbe4b16ad8178
Oleg Nikitashin <olegnikitashin@gmail.com>
2017-05-28 22:40:06 +0300

    Add comment partial

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+  before_action :set_blog, only: [:edit, :update, :destroy, :toggle_status]
   layout 'blog'
   access all: [:show, :index], user: { except: [:new, :create, :update, :edit, :destroy, :toggle_status] }, site_admin: :all
 
@@ -7,7 +7,10 @@ class BlogsController < ApplicationController
     @blogs = Blog.page(params[:page]).per(5)
   end
 
-  def show; end
+  def show
+    @blog = Blog.includes(:comments).friendly.find(params[:id])
+    @comment = Comment.new
+  end
 
   def new
     @blog = Blog.new
@@ -61,6 +64,7 @@ class BlogsController < ApplicationController
   end
 
   private
+
   def set_blog
     @blog = Blog.friendly.find(params[:id])
   end
