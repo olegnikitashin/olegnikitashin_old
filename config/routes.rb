c100fe319a349688eb179c191f4076c3aa5bdd18
Oleg Nikitashin <olegnikitashin@gmail.com>
2017-05-29 13:55:53 +0300

    Add Action Cable to Blog Comment section

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login',
                                              sign_out: 'logout',
                                              sign_up: 'register' }
@@ -20,4 +19,6 @@ Rails.application.routes.draw do
   end
 
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
+  mount ActionCable.server => '/cable'
 end
