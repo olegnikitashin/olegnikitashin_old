31d272e275ca45e45fb2f809f919c204da11c2e3
Oleg Nikitashin <olegnikitashin@gmail.com>
2017-05-29 17:28:24 +0300

    Add Blog sidebar module with Topics that have blog posts

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,8 @@
 class Topic < ApplicationRecord
   has_many :blogs
-  
+
   validates_presence_of :title
+
+  scope :with_blogs, -> { includes(:blogs)
+                              .where.not(blogs: { id: nil }) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
   devise_for :users, path: '', path_names: { sign_in: 'login',
                                              sign_out: 'logout',
                                              sign_up: 'register' }
-  root to: 'pages#home'
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   get 'tech-news', to: 'pages#tech_news'
 
+  resources :topics, only: [:index, :show]
   resources :blogs do
     member do
       get :toggle_status
     end
   end
+
 
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
